51bb9db62dc640b2d82a88b01c5c8c5bd0226de6
kdmin0706 <124044861+kdmin0706@users.noreply.github.com>
2023-08-24 19:53:12 +0900

    [level 0] Title: 가위 바위 보, Time: 0.16 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        
+        StringBuffer sb = new StringBuffer();
+        for(char c : rsp.toCharArray()){
+            if(c == '2'){
+                sb.append(0);
+            } else if(c == '0'){
+                sb.append(5);
+            } else{
+                sb.append(2);
+            }
+        }
+        
+        
+        return sb.toString();
+    }
+}
