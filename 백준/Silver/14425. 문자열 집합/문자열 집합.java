c913b9471c72663a23a6b74092532677434e6f57
kdmin0706 <124044861+kdmin0706@users.noreply.github.com>
2023-08-27 07:23:00 +0900

    [Silver III] Title: 문자열 집합, Time: 404 ms, Memory: 31156 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Set<String> set = new HashSet<>();
+
+        int count = 0;
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < N; i++) {
+            set.add(br.readLine());
+        }
+
+        for (int i = 0; i < M; i++) {
+            String s = br.readLine();
+
+            if(set.contains(s)){
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
