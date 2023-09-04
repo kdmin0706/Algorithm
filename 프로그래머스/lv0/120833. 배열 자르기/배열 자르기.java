892aaa5cde74e489f876e09593377053fd5b9392
kdmin0706 <124044861+kdmin0706@users.noreply.github.com>
2023-09-04 18:35:35 +0900

    [level 0] Title: 배열 자르기, Time: 1.83 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        List<Integer> list = new ArrayList<>();
+        
+        for (int i = 0; i < numbers.length; i++) {
+            if (i >= num1 && i <= num2) {
+                list.add(numbers[i]);
+            }            
+        }
+        
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}
