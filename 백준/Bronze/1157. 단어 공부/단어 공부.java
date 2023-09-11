436de547b63fb6f5fbbeb21ff9e266a660f246c3
kdmin0706 <124044861+kdmin0706@users.noreply.github.com>
2023-09-11 19:37:12 +0900

    [Bronze I] Title: 단어 공부, Time: 372 ms, Memory: 36680 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String s = br.readLine().toLowerCase();
+        String s = br.readLine().toUpperCase();
 
         Map<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
