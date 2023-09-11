4a9be044026f27649eeb4323aec5e19015e29b7f
kdmin0706 <124044861+kdmin0706@users.noreply.github.com>
2023-09-11 19:36:11 +0900

    [Bronze I] Title: 단어 공부, Time: 372 ms, Memory: 36740 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine().toLowerCase();
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+
+        int count = 0;
+        int max = Collections.max(map.values());
+
+        for (char key : map.keySet()) {
+            int value = map.get(key);
+
+            if (value == max) {
+                count++;
+                s = String.valueOf(key);
+            }
+        }
+        
+        System.out.println(count > 1 ? "?" : s.toUpperCase());
+    }
+}
