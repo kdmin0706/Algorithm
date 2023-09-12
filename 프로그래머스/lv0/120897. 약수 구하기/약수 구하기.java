00fa8575801f9d50c886932ced1373554cd4ba56
kdmin0706 <124044861+kdmin0706@users.noreply.github.com>
2023-09-12 22:56:31 +0900

    [level 0] Title: 약수 구하기, Time: 2.90 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int[] solution(int n) {
+        return IntStream.range(1, n + 1).filter(i -> n % i == 0).toArray();
+    }
+}
