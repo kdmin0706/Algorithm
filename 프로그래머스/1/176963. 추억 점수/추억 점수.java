5766c588e268c58a8d07af0625026db7ed751268
KYUNGDAM MIN <rudeka0706@gmail.com>
2024-04-04 00:20:16 +0900

    [level 1] Title: 추억 점수, Time: 0.03 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < name.length; i++) {
+            map.put(name[i], yearning[i]);
+        }
+
+        for (int i = 0; i < photo.length; i++) {
+            for (String string : photo[i]) {
+                answer[i] += map.getOrDefault(string, 0);
+            }
+        }
+
+        return answer;
+    }
+}
