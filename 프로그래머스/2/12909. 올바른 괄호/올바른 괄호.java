a40edddfac4b87b2d6b7c66575e911402265d478
KYUNGDAM MIN <rudeka0706@gmail.com>
2024-04-10 14:57:36 +0900

    [level 2] Title: 올바른 괄호, Time: 19.52 ms, Memory: 53.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (Character c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);   
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
+            }            
+        }
+        return stack.isEmpty();
+    }
+}
