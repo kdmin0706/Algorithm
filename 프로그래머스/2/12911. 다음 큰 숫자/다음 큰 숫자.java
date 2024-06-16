c2ed1713e6d7fe765dc95e194a353725498ed816
KYUNGDAM MIN <rudeka0706@gmail.com>
2024-06-17 08:45:58 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.03 ms, Memory: 52.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int count = Integer.bitCount(n);
+
+        int next = n + 1;
+        while (true) {
+            if (Integer.bitCount(next) == count) {
+                return next;
+            }
+            next++;
+        }
+    }
+}
