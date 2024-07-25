234789f80c6508379e4f598c4314b8b8eae1d2be
KYUNGDAM MIN <rudeka0706@gmail.com>
2024-07-25 13:39:09 +0900

    [goormlevel] Title: 여러 번 계산하기, Time: 0.14 ms, Memory: 32.04 MB, Difficulty: 1 -BaekjoonHub

---
+++ goormlevel/242348/1. 여러 번 계산하기/여러 번 계산하기.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i =0;i<N;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			System.out.println(a+b);
+		}
+				
+	}
+}
